d82b7ccf2cea1bab8d08171c3dc3079ac50a16a3
Luke B <115926691+lukebarnes-ct@users.noreply.github.com>
2023-10-06 12:49:50 +0200

    Preparing Data for Neural Networks

--- NeuralNetworks.R
+++ NeuralNetworks.R
@@ -5,108 +5,58 @@ library(tidyverse)
 library(tidytext)
 library(tokenizers)
 library(gghighlight)
-library(rpart)
 library(tictoc)
-library(ranger)
-library(caret)
+library(keras)
+library(reticulate)
+library(tensorflow)
 
 ### Load Different Datasets
 
 load("SetSentenceData.RData")
-
-## Classification Tree
-
-tic()
-bowFit = rpart(presidentName ~., 
-               data = trainingSentencesBOW,
-               method = "class")
-toc()
-
-trainFittedBOW = predict(bowFit, type = 'class')
-trainPredBOW = table(trainingSentencesBOW$presidentName, trainFittedBOW)
-round(sum(diag(trainPredBOW))/sum(trainPredBOW), 3)
-
-tic()
-tf.idf.Fit = rpart(presidentName ~., 
-                   data = trainingSentences.TF.IDF,
-                   method = "class")
-toc()
-
-trainFitted.TF.IDF = predict(tf.idf.Fit, type = 'class')
-trainPred.TF.IDF = table(trainingSentences.TF.IDF$presidentName, 
-                         trainFitted.TF.IDF)
-round(sum(diag(trainPred.TF.IDF))/sum(trainPred.TF.IDF), 3)
-
-## Random Forests
-
-trainingSentencesBOW$presidentName = factor(trainingSentencesBOW$presidentName)
-
-tic()
-bowFit.Ranger = ranger(presidentName ~ ., 
-                       data = trainingSentencesBOW,
-                       mtry = 20,
-                       num.trees = 500)
-toc()
-
-trainFittedBOW.Ranger = bowFit.Ranger$predictions
-trainPredBOW.Ranger = table(trainingSentencesBOW$presidentName, trainFittedBOW.Ranger)
-round(sum(diag(trainPredBOW.Ranger))/sum(trainPredBOW.Ranger), 3)
-
-trainingSentences.TF.IDF$presidentName = factor(trainingSentences.TF.IDF$presidentName)
-
-tic()
-tf.idf.Fit.Ranger = ranger(presidentName ~ ., 
-                           data = trainingSentences.TF.IDF,
-                           mtry = 20,
-                           num.trees = 500)
-toc()
-
-trainFitted.TF.IDF.Ranger = tf.idf.Fit.Ranger$predictions
-trainPred.TF.IDF.Ranger = table(trainingSentences.TF.IDF$presidentName, trainFitted.TF.IDF.Ranger)
-round(sum(diag(trainPred.TF.IDF.Ranger))/sum(trainPred.TF.IDF.Ranger), 3)
-
-#### Validation Analysis with Ranger
-
-error = c()
-valErr = c()
-seq = seq(250, 1750, by = 250)
-for(i in 1:length(seq)){
-  tic()
-  mod = ranger(presidentName ~ ., 
-               data = trainingSentences.TF.IDF,
-               num.trees = seq[i], 
-               mtry = 10)
-  error[i] = mod$prediction.error
-  valModPreds = predict(mod, validationSentences.TF.IDF)$predictions
-  valModPreds.Ranger = table(validationSentences.TF.IDF$presidentName, valModPreds)
-  valErr[i] = (1 - round(sum(diag(valModPreds.Ranger))/sum(valModPreds.Ranger), 3))
-  print(paste0("Iteration: ", i))
-  toc()
-}
-
-plot(seq, error, type = 'l', col = 'blue', lwd = 3, ylim = c(0.5, 0.575))
-plot(seq, valErr, type = 'l', col = 'blue', lwd = 3)
-lines(seq, valErr, col = 'red', lwd = 3)
-
-rf_grid = expand.grid(mtry = seq(5, 25, by = 5),
-                      splitrule = 'gini',
-                      min.node.size = 1)
-
-ctrl = trainControl(method = 'oob', verboseIter = T)
-
-tic()
-rf_gridsearch = train(presidentName ~ ., 
-                      data = trainingSentences.TF.IDF,
-                      method = 'ranger',
-                      num.trees = 100,
-                      verbose = T,
-                      trControl = ctrl,
-                      tuneGrid = rf_grid)
-toc()
-
-res = rf_gridsearch$results
-rf_gridsearch$finalModel
-
+set.seed(2023)
 
 ## Neural Networks
+
+# Pre-processing the Target Variable
+
+presTarget.train.BOW = as.integer(factor(trainingSentencesBOW$presidentName)) - 1
+presTarget.train.TF.IDF = as.integer(factor(trainingSentences.TF.IDF$presidentName)) - 1
+
+presTarget.val.BOW = as.integer(factor(validationSentencesBOW$presidentName)) - 1
+presTarget.val.TF.IDF = as.integer(factor(validationSentences.TF.IDF$presidentName)) - 1
+
+presTarget.test.BOW = as.integer(factor(testSentencesBOW$presidentName)) - 1
+presTarget.test.TF.IDF = as.integer(factor(testSentences.TF.IDF$presidentName)) - 1
+
+# Removing President Variable from Data sets and scale Bag-Of-Words Data
+
+presData.train.BOW = trainingSentencesBOW %>% 
+  select(-presidentName) %>% 
+  scale()
+
+presData.train.TF.IDF = trainingSentences.TF.IDF %>% 
+  select(-presidentName) %>%
+  as.matrix()
+
+presData.val.BOW = validationSentencesBOW %>% 
+  select(-presidentName) %>%
+  scale(center = attr(presData.train.BOW, "scaled:center"), 
+        scale = attr(presData.train.BOW, "scaled:scale"))
+
+presData.val.TF.IDF = validationSentences.TF.IDF %>% 
+  select(-presidentName) %>%
+  as.matrix()
+
+presData.test.BOW = testSentencesBOW %>% 
+  select(-presidentName) %>%
+  scale(center = attr(presData.train.BOW, "scaled:center"), 
+        scale = attr(presData.train.BOW, "scaled:scale"))
+
+presData.test.TF.IDF = testSentences.TF.IDF %>% 
+  select(-presidentName) %>%
+  as.matrix()
+
+
+
+
 
