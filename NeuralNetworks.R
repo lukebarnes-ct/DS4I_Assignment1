d4924951760d620c78f4769296ea4b089bd86dbf
Luke B <115926691+lukebarnes-ct@users.noreply.github.com>
2023-10-06 14:50:10 +0200

    Attempting to implement a Feed-Forward Neural Network for the Bag-Of-Words data

--- NeuralNetworks.R
+++ NeuralNetworks.R
@@ -56,7 +56,42 @@ presData.test.TF.IDF = testSentences.TF.IDF %>%
   select(-presidentName) %>%
   as.matrix()
 
+# One-Hot Encoding President Name variable
 
+presTarget.train.BOW.OH = to_categorical(presTarget.train.BOW)
+presTarget.train.TF.IDF.OH = to_categorical(presTarget.train.TF.IDF)
 
+presTarget.val.BOW.OH = to_categorical(presTarget.val.BOW)
+presTarget.val.TF.IDF.OH = to_categorical(presTarget.val.TF.IDF)
 
+presTarget.test.BOW.OH = to_categorical(presTarget.test.BOW)
+presTarget.test.TF.IDF.OH = to_categorical(presTarget.test.TF.IDF)
+
+# Creating Feed-Forward Neural Network Model
+
+dimensions = dim(presData.train.BOW)
+presNum = dim(presTarget.test.BOW.OH)[2]
+
+bowFit.FF.NN1 = keras_model_sequential()
+
+bowFit.FF.NN1 %>% 
+  layer_dense(units = dimensions[1]/10, 
+              activation = "relu", 
+              input_shape = dimensions[2]) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_dense(units = presNum, activation = "softmax")
+
+bowFit.FF.NN1 %>% compile(
+  loss = "categorical_crossentropy",
+  optimizer = optimizer_adam(learning_rate = 0.01),
+  metrics = c('accuracy'),
+)
+
+bowFit.FF.NN1.History = bowFit.FF.NN1 %>% fit(
+  presData.train.BOW, presTarget.train.BOW.OH, 
+  epochs = 10, batch_size = 10,
+  #validation_split = 0.1,
+  validation_data = list(presData.val.BOW, presTarget.val.BOW.OH),
+  verbose = 1, shuffle = TRUE
+)
 
